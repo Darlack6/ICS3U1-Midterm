857e90e1b99a39886e86f2e0a05f63cc5ab6c268
Darlack6 <78220630+Darlack6@users.noreply.github.com>
2023-04-19 11:50:15 -0400

    Scene 2

--- midtermproject.java
+++ midtermproject.java
@@ -13,20 +13,20 @@ public class midtermproject {
 
 }
 	public static void introscene1(Console con){
-		char keypress;
+		char charKeypress;
 		Font montserrat = con.loadFont("Montserrat-SemiBold.ttf", 30); 
 		con.setTextFont(montserrat);
 		con.clear();
 		con.println("\n\n\n\n\n      The airport is booming, after all, the golden age of air travel just ended.\nThe year is 1971. Air travel is becoming more popular and affordable for civilians.\n\n                             But today, this flight is not your typical A to B flight. \n\nGoing through security, you start getting scared. If the guards realize, your plan\n                        that you spent years working out would be ruined.");
 		con.drawString("Press [space] to continue",470,650);
-		keypress=con.getChar();
-		if(keypress==' '){
+		charKeypress=con.getChar();
+		if(charKeypress==' '){
 			con.clear();
 			con.println("\n\n\n   As you approach the scanner, you start fiddling with the coins in your pocket.\nYou hesitantly place your luggage and briefcase on the belt and hope for the best. \n\nThe luggage disappears under the machine. You walk through the screener,\n          but it starts beeping. You get worried as you run through scenarios of\n                                                        what went wrong.\n\n                    But then you remember you still had coins in your pocket.\n       After depositing the change, you walk through the screener and are cleared.\n\n                                      Your luggage also passed the machine.\n\n                                                 You let out a sigh of relief.");
-			keypress='i';
+			charKeypress='i';
 		}
-		keypress=con.getChar();
-		if(keypress==' '){
+		charKeypress=con.getChar();
+		if(charKeypress==' '){
 			con.clear();
 			con.setDrawColor(Color.BLACK);
 			con.fillRect(470,650,500,300);
@@ -39,14 +39,17 @@ public class midtermproject {
 	}
 	
 	public static void scene1(Console con){
-		char keypress;
+		char charKeypress;
 		Font montserrat = con.loadFont("Montserrat-SemiBold.ttf", 30); 
 		BufferedImage scene2 = con.loadImage("scene2.png");
 		con.setTextFont(montserrat);
 		con.println("\n\n\n\n\n\n                       Tomorrow is the big day, a day you've been planning for years,\n                             but first, you need to get the bomb from the safe"); 
 		con.drawString("Press [space] to continue",470,650);
-		keypress=con.getChar();
-		if(keypress==' '){
+		charKeypress=con.getChar();
+		if(charKeypress==' '){
+			int intRandomnum1=(int)(Math.random() * 2+1);
+			int intRandomnum2=(int)(Math.random() * 2+1);
+			int intx = 0;
 			con.clear();
 			con.drawImage(scene2,0,0);
 			con.setDrawColor(Color.WHITE);
@@ -55,8 +58,23 @@ public class midtermproject {
 			con.setDrawColor(Color.BLACK);
 			con.setDrawFont(montserrat);
 			con.drawString("To unlock the safe, you", 25, 25);
-			con.drawString("need to solve a math",25,45);
-			con.drawString("question",25,65);
+			con.drawString("need to solve a math",25,50);
+			con.drawString("question",25,75);
+			con.drawString("Solve for x",25,125);
+			con.drawString("x = "+intRandomnum1+" * "+intRandomnum2,25,150);
+			montserrat = con.loadFont("Montserrat-SemiBold.ttf", 19);
+			con.setTextFont(montserrat);
+			con.setTextColor(Color.GREEN);
+			con.print("\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                                                          ");
+			while(intx!=intRandomnum1*intRandomnum2){
+				intx=con.readInt();
+				if(intx==intRandomnum1*intRandomnum2){
+					con.clear();
+					con.drawString("Correct!",100,100);
+				}else{
+					con.drawString("Incorrect, try again",100,100);
+				}
+			}
 			
 		}
 	}
