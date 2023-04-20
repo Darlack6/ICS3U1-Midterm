5b7b3d8da42e12ec6b12623778746179bee2e905
Darlack6 <78220630+Darlack6@users.noreply.github.com>
2023-04-19 21:30:45 -0400

    Added circle transition and further refinements

--- midtermproject.java
+++ midtermproject.java
@@ -7,22 +7,26 @@ public class midtermproject {
 	public static void main (String[] args) {
 		Console con = new Console("The Plane Hijack",1280,720);
 		scene1(con);
-		//introscene1(con);
-		//slidetransition(con);
+		con.sleep(2000);
+		circletransition(con);
+		plotscene(con);
+		slidetransition(con);
 
 
 }
-	public static void introscene1(Console con){
+	public static void plotscene(Console con){
 		char charKeypress;
 		Font montserrat = con.loadFont("Montserrat-SemiBold.ttf", 30); 
 		con.setTextFont(montserrat);
+		con.setTextColor(Color.WHITE);
+		con.setDrawColor(Color.WHITE);
 		con.clear();
-		con.println("\n\n\n\n\n      The airport is booming, after all, the golden age of air travel just ended.\nThe year is 1971. Air travel is becoming more popular and affordable for civilians.\n\n                             But today, this flight is not your typical A to B flight. \n\nGoing through security, you start getting scared. If the guards realize, your plan\n                        that you spent years working out would be ruined.");
+		con.println("\n\n\n\n\n      The airport is booming, after all, the golden age of air travel just ended.\n  The year is 1971. Air travel is becoming more popular and affordable for civilians.\n\n                             But today, this flight is not your typical A to B flight. \n\n  Going through security, you start getting scared. If the guards realize, your plan\n                          that you spent years working out would be ruined.");
 		con.drawString("Press [space] to continue",470,650);
 		charKeypress=con.getChar();
 		if(charKeypress==' '){
 			con.clear();
-			con.println("\n\n\n   As you approach the scanner, you start fiddling with the coins in your pocket.\nYou hesitantly place your luggage and briefcase on the belt and hope for the best. \n\nThe luggage disappears under the machine. You walk through the screener,\n          but it starts beeping. You get worried as you run through scenarios of\n                                                        what went wrong.\n\n                    But then you remember you still had coins in your pocket.\n       After depositing the change, you walk through the screener and are cleared.\n\n                                      Your luggage also passed the machine.\n\n                                                 You let out a sigh of relief.");
+			con.println("\n\n\n   As you approach the scanner, you start fiddling with the coins in your pocket.\nYou hesitantly place your luggage and briefcase on the belt and hope for the best. \n\n   The luggage disappears under the machine. You walk through the screener,\n          but it starts beeping. You get worried as you run through scenarios of\n                                                        what went wrong.\n\n                    But then you remember you still had coins in your pocket.\n       After depositing the change, you walk through the screener and are cleared.\n\n                                      Your luggage also passed the machine.\n\n                                                 You let out a sigh of relief.");
 			charKeypress='i';
 		}
 		charKeypress=con.getChar();
@@ -53,7 +57,7 @@ public class midtermproject {
 			con.clear();
 			con.drawImage(scene2,0,0);
 			con.setDrawColor(Color.WHITE);
-			con.fillRoundRect(20,15,300,200,10,10);
+			con.fillRoundRect(20,15,300,170,10,10);
 			montserrat = con.loadFont("Montserrat-SemiBold.ttf", 23); 
 			con.setDrawColor(Color.BLACK);
 			con.setDrawFont(montserrat);
@@ -70,19 +74,16 @@ public class midtermproject {
 				con.clear();
 				con.print("\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                                                          ");
 				intx=con.readInt();
+				montserrat = con.loadFont("Montserrat-SemiBold.ttf", 19);
+				con.setTextFont(montserrat);
 				if(intx==intRandomnum1*intRandomnum2){
 					con.clear();
-					montserrat = con.loadFont("Montserrat-SemiBold.ttf", 18);
-					con.setDrawFont(montserrat);
-					con.drawString("Correct!",525,308);
+					con.println("\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                                                          Correct!");
 				}else{
 					con.clear();
-					montserrat = con.loadFont("Montserrat-SemiBold.ttf", 18);
-					con.setDrawFont(montserrat);
-					con.drawString("Incorrect",525,308);
+					con.println("\n\n\n\n\n\n\n\n\n\n\n\n\n                                                                                                          Incorrect");
 					con.sleep(1000);
-					con.setDrawColor(Color.BLACK);
-					con.drawRect(525,308,20,20);
+					con.clear();
 				}
 			}
 			
@@ -93,9 +94,24 @@ public class midtermproject {
 		int intcounter;
 		con.setDrawColor(Color.BLACK);
 		con.sleep(4000);
-		for(intcounter=0;intcounter<1280;intcounter++){
+		con.clear();
+		for(intcounter=0;intcounter<1281;intcounter++){
 			con.fillRect(0,0,intcounter,720);
 			con.repaint();
+			con.sleep(1);
+		}
+	}
+
+	public static void circletransition(Console con){
+		int intcounter;
+		int intI=0;
+		con.setDrawColor(Color.BLACK);
+		con.sleep(3000);
+		con.clear();
+		for(intcounter=0;intcounter<1500;intcounter=intcounter+2){
+			con.fillOval(640-intI,370-intI,intcounter,intcounter);
+			con.repaint();
+			intI++;
 			con.sleep(1);
 		}
 	}
